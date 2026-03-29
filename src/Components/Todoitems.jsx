ee52aead3cd8ae503f67a7cd6d7d19c01dca032e
OrlandoJim24 <orlandojim2023@gmail.com>
2026-03-28 22:33:50 -0400

    Added Todoitems component with basic structure

---
+++ src/Components/Todoitems.jsx
@@ -0,0 +1,11 @@
+ import './CSS/Todoitems.css';
+
+ const Todoitems = () => {
+  return (
+    <div className='todo'>
+        <div className="todo-header"></div>
+    </div>
+  )
+}
+
+export default Todoitems;
