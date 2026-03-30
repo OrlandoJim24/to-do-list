a571f8b05dec45ede50d0cbbc4c4cde672409312
OrlandoJim24 <orlandojim2023@gmail.com>
2026-03-30 08:41:34 -0400

    Added Todoitems component with tick, non-tick and cross assets

--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,8 +1,19 @@
 import './CSS/Todoitems.css'
+import tick from '../Assets/tick.png'
+import non_tick from '../Assets/non_tick.png'
+import cross from '../Assets/cross.png'
 
 const Todoitems = () => {
   return (
-    <div>Todoitems</div>
+    <div className='todoitems'>
+        <div className="todoitems-container">
+            <img src={non_tick} alt="Tick"/>
+            <img src={tick} alt="Non-Tick"/>
+            <div className="todoitems-text"></div>
+
+        </div>
+        <img src={cross} alt=""/>
+    </div>
   )
 }
 
